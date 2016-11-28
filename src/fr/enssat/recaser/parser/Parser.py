0f562acbf9a60f56485e89eb604671a6ad99abd8
Anais Galisson <anais.galisson@gmail.com>
2016-11-28 12:01:00 +0100

    On a mis l'id

--- src/fr/enssat/recaser/parser/Parser.py
+++ src/fr/enssat/recaser/parser/Parser.py
@@ -4,7 +4,7 @@ from src.fr.enssat.recaser.parser.RecaserOperation import RecaserOperation
 from src.fr.enssat.recaser.parser.SentenceElement import SentenceElement
 
 
-class Parser(object) :
+class Parser(object):
     WORD = "word"
     WORD_NLTK = "word_nltk"
     CHARACTER = "char"
@@ -13,17 +13,17 @@ class Parser(object) :
     # CONSTRUCTOR
     # ===========
 
-    def __init__(self, mode = "word") :
+    def __init__(self, mode="word"):
         self.mode = mode
 
     # ================
     # PUBLIC FUNCTIONS
     # ================
 
-    def read(self, file_name) :
-        if self.mode == self.WORD :
+    def read(self, file_name):
+        if self.mode == self.WORD:
             return self.__readAsWord(file_name)
-        elif self.mode == self.CHARACTER :
+        elif self.mode == self.CHARACTER:
             return self.__readAsChar(file_name)
         else:
             return self.__readAsWordNLTK(file_name)
@@ -32,20 +32,21 @@ class Parser(object) :
     # PRIVATE METHODS
     # ===============
 
-    def __readAsWord(self, file_name) :
+    #@deprecated
+    def __readAsWord(self, file_name):
         from string import punctuation
 
         pattern = regex.compile(r'\w+|[{}]'.format(regex.escape(punctuation)))
         elements = []
-        with open(file_name, 'r') as file :
-            for line in file :
+        with open(file_name, 'r') as file:
+            for line in file:
                 parts = pattern.findall(line)
-                for part in parts :
-                    if part.isupper() :
+                for part in parts:
+                    if part.isupper():
                         operation = RecaserOperation.FULL_UPPER
-                    elif part[0].isupper() :
+                    elif part[0].isupper():
                         operation = RecaserOperation.START_UPPER
-                    else :
+                    else:
                         operation = RecaserOperation.NOTHING
 
                     element = SentenceElement(part, operation)
@@ -53,17 +54,34 @@ class Parser(object) :
 
         return elements
 
-    def __readAsWordNLTK(self, file_name) :
+    def __readAsWordNLTK(self, file_name):
         from nltk import word_tokenize
         from nltk import pos_tag
+        elements = []
 
         text = ""
         with open(file_name, 'r') as file:
             for line in file:
                 text += line
+        tokens_tags = pos_tag(word_tokenize(text))
 
-        tokens = word_tokenize(text)
-        return pos_tag(tokens)
+        for token in tokens_tags:
+            if token[0].isupper():
+                operation = RecaserOperation.FULL_UPPER
+            elif token[0][0].isupper():
+                operation = RecaserOperation.START_UPPER
+            else:
+                operation = RecaserOperation.NOTHING
+
+            token_tmp = self.replace_at_index1(token, 1, token[0].lower())
+            element = SentenceElement(token_tmp, operation)
+
+            for existing in elements:
+                if existing.value[0] == token[0].lower():
+                    element.id = existing.id
+            elements.append(element)
+
+        return elements
 
     def __readAsChar(self, file_name):
         with open(file_name, 'r') as file:
@@ -73,9 +91,14 @@ class Parser(object) :
                 for item in tmp:
                     if item.isupper():
                         operation = RecaserOperation.START_UPPER
-                    else :
+                    else:
                         operation = RecaserOperation.NOTHING
 
                     element = SentenceElement(item, operation)
                     elements.append(element)
         return elements
+
+    def replace_at_index1(self, tup, index, val):
+        lst = list(tup)
+        lst[index] = val
+        return tuple(lst)
--- src/fr/enssat/recaser/tests/TestNLTK.py
+++ src/fr/enssat/recaser/tests/TestNLTK.py
@@ -1,10 +1,13 @@
-from nltk import word_tokenize, pos_tag
+
+from src.fr.enssat.recaser.parser.Parser import Parser
+
+def getAbsolutePath(file_name) :
+    """Compute the absolute path of the file if present in the 'resources' directory"""
+    import os
+    basepath = os.path.dirname(__file__)
+    return os.path.abspath(os.path.join(basepath, "..", "..", "..", "..", "..", "resources", file_name))
 
 if __name__ == "__main__" :
     text = "Machine learning is the science of getting computers to act without being explicitly programmed. In the past decade, machine learning has given us self-driving cars, practical speech recognition, effective web search, and a vastly improved understanding of the human genome. Machine learning is so pervasive today that you probably use it dozens of times a day without knowing it. Many researchers also think it is the best way to make progress towards human-level AI. In this class, you will learn about the most effective machine learning techniques, and gain practice implementing them and getting them to work for yourself. More importantly, you'll learn about not only the theoretical underpinnings of learning, but also gain the practical know-how needed to quickly and powerfully apply these techniques to new problems. Finally, you'll learn about some of Silicon Valley's best practices in innovation as it pertains to machine learning and AI."
-
-    tokens = word_tokenize(text)
-    print(tokens)
-
-    tagged_tokens = pos_tag(tokens)
-    print(tagged_tokens)
+    parser = Parser(Parser.WORD_NLTK)
+    parser.read(getAbsolutePath("test.txt"))
