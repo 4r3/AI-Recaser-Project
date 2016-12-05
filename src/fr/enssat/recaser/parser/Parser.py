334d5909fa70d482183ae5ff0bb399847ee9c424
hiveship <maelig.nantel@gmail.com>
2016-12-05 11:45:19 +0100

    push mais ça marche pas

--- src/fr/enssat/recaser/parser/Parser.py
+++ src/fr/enssat/recaser/parser/Parser.py
@@ -1,11 +1,9 @@
-import re as regex
 
 from src.fr.enssat.recaser.parser.RecaserOperation import RecaserOperation
 from src.fr.enssat.recaser.parser.SentenceElement import SentenceElement
 
 
-class Parser(object):
-    WORD = "word"
+class Parser(object) :
     WORD_NLTK = "word_nltk"
     CHARACTER = "char"
 
@@ -13,87 +11,103 @@ class Parser(object):
     # CONSTRUCTOR
     # ===========
 
-    def __init__(self, mode="word"):
+    def __init__(self, mode = "word") :
         self.mode = mode
 
     # ================
     # PUBLIC FUNCTIONS
     # ================
 
-    def read(self, file_name, isFile=False):
-        if self.mode == self.WORD:
-            return self.__readAsWord(file_name)
-        elif self.mode == self.CHARACTER:
-            return self.__readAsChar(file_name)
-        else:
-            return self.__readAsWordNLTK(file_name)
+    def read(self, content, isFile = False) :
+        if self.mode == self.CHARACTER and isFile == False:
+            return self.__readAsCharText(content)
+        elif self.mode == self.CHARACTER and isFile == True:
+            return self.__readAsCharFile(content)
+        elif self.mode == self.WORD_NLTK and isFile == False:
+            return self.__readAsWordNLTKText(content)
+        elif self.mode == self.WORD_NLTK and isFile == True:
+            return self.__readAsWordNLTKText(content)
 
     # ===============
     # PRIVATE METHODS
     # ===============
 
-    #@deprecated
-    def __readAsWord(self, file_name):
-        from string import punctuation
-
-        pattern = regex.compile(r'\w+|[{}]'.format(regex.escape(punctuation)))
-        elements = []
-        with open(file_name, 'r') as file:
-            for line in file:
-                parts = pattern.findall(line)
-                for part in parts:
-                    if part.isupper():
-                        operation = RecaserOperation.FULL_UPPER
-                    elif part[0].isupper():
-                        operation = RecaserOperation.START_UPPER
-                    else:
-                        operation = RecaserOperation.NOTHING
-
-                    element = SentenceElement(part, operation)
-                    elements.append(element)
-
-        return elements
-
-    def __readAsWordNLTK(self, file_name):
+    def __readAsWordNLTKFile(self, file_name) :
         from nltk import word_tokenize
         from nltk import pos_tag
         elements = []
 
         text = ""
-        with open(file_name, 'r') as file:
-            for line in file:
+        with open(file_name, 'r') as file :
+            for line in file :
                 text += line
         tokens_tags = pos_tag(word_tokenize(text))
 
-        for token in tokens_tags:
-            if token[0].isupper():
+        for token in tokens_tags :
+            if token[0].isupper() :
                 operation = RecaserOperation.FULL_UPPER
-            elif token[0][0].isupper():
+            elif token[0][0].isupper() :
                 operation = RecaserOperation.START_UPPER
-            else:
+            else :
                 operation = RecaserOperation.NOTHING
 
             element = SentenceElement(token[0].lower(), token[1], operation)
 
-            for existing in elements:
-                if existing.value[0] == token[0].lower():
+            for existing in elements :
+                if existing.value[0] == token[0].lower() :
                     element.id = existing.id
             elements.append(element)
 
         return elements
 
-    def __readAsChar(self, file_name):
-        with open(file_name, 'r') as file:
+    def __readAsWordNLTKText(self, text) :
+        from nltk import word_tokenize
+        from nltk import pos_tag
+        elements = []
+
+        tokens_tags = pos_tag(word_tokenize(text))
+
+        for token in tokens_tags :
+            if token[0].isupper() :
+                operation = RecaserOperation.FULL_UPPER
+            elif token[0][0].isupper() :
+                operation = RecaserOperation.START_UPPER
+            else :
+                operation = RecaserOperation.NOTHING
+
+            element = SentenceElement(token[0].lower(), token[1], operation)
+
+            for existing in elements :
+                if existing.value[0] == token[0].lower() :
+                    element.id = existing.id
+            elements.append(element)
+
+        return elements
+
+    def __readAsCharFile(self, file_name) :
+        with open(file_name, 'r') as file :
             elements = []
-            for line in file:
+            for line in file :
                 tmp = list(line)
-                for item in tmp:
-                    if item.isupper():
+                for item in tmp :
+                    if item.isupper() :
                         operation = RecaserOperation.START_UPPER
-                    else:
+                    else :
                         operation = RecaserOperation.NOTHING
 
-                    element = SentenceElement(item, operation)
+                    element = SentenceElement(item, None, operation)
                     elements.append(element)
         return elements
 
+    def __readAsCharText(self, text) :
+        elements = []
+        for item in text :
+            if item.isupper() :
+                operation = RecaserOperation.START_UPPER
+            else :
+                operation = RecaserOperation.NOTHING
+
+            element = SentenceElement(item, None, operation)
+            elements.append(element)
+
+        return elements
