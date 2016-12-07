1055904632f6c658bb0bf4f8f8a76d791a32e95f
hiveship <maelig.nantel@gmail.com>
2016-12-07 17:36:41 +0100

    refacto restorator

---
+++ src/fr/enssat/recaser/RecaserMethod.py
@@ -0,0 +1,5 @@
+class RecaserMethod(enumerate) :
+    DNN_CHAR = 0
+    DNN_WORD = 1
+    CRF_CHAR = 2
+    CRF_WORD = 3
--- src/fr/enssat/recaser/validation/Restorator.py
+++ src/fr/enssat/recaser/validation/Restorator.py
@@ -1,24 +1,33 @@
+
 from src.fr.enssat.recaser.DNN.CharDNNRecaser import CharDNNRecaser
+from src.fr.enssat.recaser.RecaserMethod import RecaserMethod
 
 
 class Restorator(object) :
-    def restore(self, text) :
-        text = text.lower()
-        recaser = CharDNNRecaser()
 
-        recaser.learn()
+    def restore(self, text_query, method) :
+        text_query = text_query.lower() # Insure it's full lower case
 
-        results = recaser.predict(text)
+        if method == RecaserMethod.DNN_CHAR:
+            recaser = CharDNNRecaser()
+            recaser.learn()
 
-        print(results)
+            results = recaser.predict(text_query)
+            text_result = ""
+            current_index = 0
+            for letter in text_query :
+                if results[current_index] == 1 :
+                    text_result = text_result + letter.upper()
+                else :
+                    text_result = text_result + letter
+                current_index += 1
 
-        text_result = ""
-        current_index = 0
-        for letter in text :
-            if results[current_index] == 1 :
-                text_result = text_result + letter.upper()
-            else :
-                text_result = text_result + letter
-            current_index += 1
+            return text_result
 
-        return text_result
+        # TODO: Handle other methods
+        else:
+            raise Exception("Invalid recaser method")
+
+
+
+
