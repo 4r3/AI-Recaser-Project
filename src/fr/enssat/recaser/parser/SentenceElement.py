be580024abec07b78aa0df2d2642d2649f0f4462
hiveship <maelig.nantel@gmail.com>
2016-12-08 12:40:12 +0100

    Merge parsebis

--- src/fr/enssat/recaser/parser/SentenceElement.py
+++ src/fr/enssat/recaser/parser/SentenceElement.py
@@ -8,15 +8,17 @@ class SentenceElement(object) :
     # CONSTRUCTOR
     # ===========
 
-    def __init__(self, value, tag, operation) :
+    def __init__(self, value, tag, operation,tag_bin, tag_bin_index) :
         self.id = next(self.last_id)
         self.value = value
         self.tag = tag
         self.operation = operation  # Operation to do
+        self.tag_bin_array = tag_bin
+        self.tag_bin_index = tag_bin_index
 
     # =========
     # UTILITIES
     # =========
 
     def __str__(self) :
-        return "SentenceElement[ id = " + str(self.id) + " | value = " + self.value + " | tag = " + str(self.tag) + " | operation = " + str(self.operation) + " ]"
+        return "SentenceElement[ id = " + str(self.id) + " | value = " + self.value + " | tag = " + str(self.tag) + " | operation = " + str(self.operation)  + " |tag binaire index : " + str(self.tag_bin_index) + " ]"
