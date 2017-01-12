8032b12b961abfc8be38cf999b24b7cb03c1b4c2
Maelig Nantel <maelig.nantel@gmail.com>
2017-01-12 16:27:44 +0100

    Update Demo.py

--- src/fr/enssat/recaser/Demo.py
+++ src/fr/enssat/recaser/Demo.py
@@ -6,14 +6,14 @@ if __name__ == "__main__" :
 
     # Recaser.recase("i love unicorns! and you, do you like unicorns? yes!", RecaserMethod.DNN_CHAR,"corpus_1/corpus")
     # Recaser.recase("i love unicorns! and you, do you like unicorns? yes!", RecaserMethod.DNN_WORD,"corpus_1/corpus")
-    # Recaser.recase("You love unicorns so much! And you, do you want to have sex with unicorns? Oh, yes James!", RecaserMethod.CRF_CHAR, ["corpus_11/corpus", "corpus_12/corpus"])
+    # Recaser.recase("You love unicorns so much!", RecaserMethod.CRF_CHAR, ["corpus_11/corpus", "corpus_12/corpus"])
     # Recaser.recase("i love unicorns! and you, do you like unicorns? yes!", RecaserMethod.DNN_WORD,"corpus_1/corpus")
     # Recaser.recase("i love unicorns! and you, do you like unicorns? yes!", RecaserMethod.CRF_CHAR,"corpus_1/corpus")
     # Recaser.recase("i love unicorns! and you, do you like unicorns? yes!", RecaserMethod.CRF_WORD,"corpus_1/corpus")
 
     #Recaser.learn(RecaserMethod.CRF_WORD, ["corpus_11/corpus", "corpus_12/corpus"], "CRFTest")
-    #print(Recaser.predict(RecaserMethod.CRF_WORD, "I love unicorns so much! And you, do you want to have sex with unicorns? Oh, yes James!", "CRFTest"))
+    #print(Recaser.predict(RecaserMethod.CRF_WORD, "I love unicorns so much!", "CRFTest"))
 
-    #print(Recaser.learn_predict(RecaserMethod.CRF_WORD, ["corpus_11/corpus", "corpus_12/corpus"], "I love unicorns so much! And you, do you want to have sex with unicorns? Oh, yes James!", "CRFTest2"))
+    #print(Recaser.learn_predict(RecaserMethod.CRF_WORD, ["corpus_11/corpus", "corpus_12/corpus"], "I love unicorns so much!", "CRFTest2"))
 
     Recaser.evaluate(RecaserMethod.CRF_WORD, ["corpus_11/corpus", "corpus_12/corpus"], TextLoader.get_text('corpus_13/corpus'))
